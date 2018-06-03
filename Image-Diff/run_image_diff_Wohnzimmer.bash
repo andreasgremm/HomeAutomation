33cfebf306ca082fefceef299e7cf4af0d8818b3
Andreas Gremm <andreas.gremm@t-online.de>
2018-06-03 13:46:18 +0200

    Image-Diff completed

---
+++ Image-Diff/run_image_diff_Wohnzimmer.bash
@@ -0,0 +1,27 @@
+#!/bin/bash
+#
+####
+#
+#     Start Image_Diff for Wohnzimmer_Cam on the Homeautomation Server
+#
+mount /mnt/synology
+mount /mnt/mediencenter
+
+if [ ! -d "/mnt/mediencenter/Hochgeladen" ]; then
+  echo "Input Directory not found"
+  umount /mnt/synology
+  umount /mnt/mediencenter
+  exit
+fi
+
+if [ ! -d "/mnt/synology/Wohnzimmer_Diffs" ]; then
+  echo "Output Directory not found"
+  umount /mnt/synology
+  umount /mnt/mediencenter
+  exit
+fi
+
+Image_diff.bash /mnt/mediencenter/Hochgeladen /mnt/synology/Wohnzimmer_Diffs
+
+umount /mnt/synology
+umount /mnt/mediencenter
