d13e65d5aff5b5826b707e850c11199666832901
Andreas Gremm <andreas.gremm@t-online.de>
2024-12-27 16:19:22 +0100

    Neue Version MFRC522

--- DockerEnv/BASH_ImageDiff/Image_diff.bash
+++ DockerEnv/BASH_ImageDiff/Image_diff.bash
@@ -12,8 +12,8 @@ then
   exit 1
 fi
 
-imagedir=$1
-diffdir=$2
+imagedir="$1"
+diffdir="$2"
 
 declare -i piccount
 declare -i diffpic
@@ -22,49 +22,49 @@ declare -i diffvalue
 piccount=0
 diffpic=0
 
-if [ -f $imagedir/Thumbs.db ]
+if [ -f "$imagedir"/Thumbs.db ]
 then
-	rm -f $imagedir/Thumbs.db
+	rm -f "$imagedir"/Thumbs.db
 fi
 
-for i in $( find $imagedir -type f -name '*.jpg' | sort ); do 
+for i in $( find "$imagedir" -type f -name '*.jpg' | sort ); do 
 	if [ $piccount -eq 0 ]
 	then
-		pic1=`basename $i`
+		pic1=$(basename "$i")
 	fi
 
 	piccount+=1
 
 	if [ $piccount -ge 2 ]
 	then
-		pic2=`basename $i`
+		pic2=$(basename "$i")
 	fi
 
 	if [ $piccount -ge 2 ]
 	then
-		echo "Verarbeite :" $pic1 $pic2
+		echo "Verarbeite :" "$pic1" "$pic2"
 
-		diffvalue=`convert -compare -metric AE -fuzz 15% $imagedir/$pic1 $imagedir/$pic2 -format "%[distortion]" info:`
+		diffvalue=$(convert -compare -metric AE -fuzz 15% "$imagedir"/"$pic1" "$imagedir"/"$pic2" -format "%[distortion]" info:)
 
 		if [ $diffvalue -gt 100 ]
 		then
 
-			if [ ! -f $diffdir/$pic1 ]
+			if [ ! -f "$diffdir"/"$pic1" ]
 			then
-				insstring=$( echo $pic1  | cut -f 2 -d"_" | cut -c1-14 )
-				convert $imagedir/$pic1 -quality 100 -auto-gamma -gravity SouthEast -font Liberation-Mono -fill yellow -annotate 0 "$insstring" $diffdir/$pic1
+				insstring=$(echo "$pic1"  | cut -f 2 -d"_" | cut -c1-14)
+				convert "$imagedir"/"$pic1" -quality 100 -auto-gamma -gravity SouthEast -font Liberation-Mono -fill yellow -annotate 0 "$insstring" "$diffdir"/"$pic1"
 			fi
-			rm -f $imagedir/$pic1
+			rm -f "$imagedir"/"$pic1"
 
-			if [ ! -f $diffdir/$pic2 ]
+			if [ ! -f "$diffdir"/"$pic2" ]
 			then
-				insstring=$( echo $pic2  | cut -f 2 -d"_" | cut -c1-14 )
-				convert $imagedir/$pic2 -quality 100 -auto-gamma -gravity SouthEast -font Liberation-Mono -fill yellow -annotate 0 "$insstring" $diffdir/$pic2
+				insstring=$( echo "$pic2"  | cut -f 2 -d"_" | cut -c1-14 )
+				convert "$imagedir"/"$pic2" -quality 100 -auto-gamma -gravity SouthEast -font Liberation-Mono -fill yellow -annotate 0 "$insstring" "$diffdir"/"$pic2"
 			fi
 
 			diffpic+=1
 		else
-			rm -f $imagedir/$pic1
+			rm -f "$imagedir"/"$pic1"
 		fi
 
 		pic1=$pic2
--- DockerEnv/BASH_Pictures2Video/pictures2video.bash
+++ DockerEnv/BASH_Pictures2Video/pictures2video.bash
@@ -11,18 +11,18 @@ then
   exit 1
 fi
 
-imagedir=$1
-videodir=$2
-videoname=$3_`date +%F_%H-%M`.mp4
+imagedir="$1"
+videodir="$2"
+videoname="$3"_$(date +%F_%H-%M).mp4
 
 pushd $imagedir
 
 ls | grep jpg >alle_bilder.txt
-ffmpeg  -framerate 1 -pattern_type glob  -i '*.jpg' -c:v libx264 -r 2 -pix_fmt yuv420p $videodir/$videoname
+ffmpeg  -framerate 1 -pattern_type glob  -i '*.jpg' -c:v libx264 -r 2 -pix_fmt yuv420p "$videodir"/"$videoname"
 
-for i in `cat alle_bilder.txt`
+for i in $(cat alle_bilder.txt)
 do
-   rm $i
+   rm "$i"
 done
 rm alle_bilder.txt
 
